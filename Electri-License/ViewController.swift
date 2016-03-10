ed66bebd795f5e1da89457fc1c9f7c4438cf5e26
Jim Witterschein <james.witterschein@gmail.com>
2016-03-09 21:46:10 -0500

    The complete stateexampass.com web wrapper app, with FaceBook button.

--- Electri-License/ViewController.swift
+++ Electri-License/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        webView = WKWebView()
+        container.addSubview(webView)
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        let frame = CGRectMake(0, 0, container.bounds.width, container.bounds.height)
+        webView.frame = frame
+        
+        loadRequest("https://www.stateexampass.com/")
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadRequest(urlStr: String) {
+        
+        let url = NSURL(string: urlStr)!
+        let request = NSURLRequest(URL: url)
+        webView.loadRequest(request)
+    }
 
+    @IBAction func loadStateExamPass(sender: AnyObject) {
+        loadRequest("https://www.stateexampass.com/")
+    }
 
+    @IBAction func loadFaceBook(sender: AnyObject) {
+        loadRequest("https://www.facebook.com/")
+    }
 }
 
